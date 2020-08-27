aa2ea4538f175bf5b6a06dbb89caf9afe0de7c49
Jake Schaffer <jakecr8guru@gmail.com>
2020-08-27 18:13:39 -0500

    added scss to make a grid layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,37 +15,20 @@ const IndexPage = () => {
         <title>Home Page</title>
       </Helmet>
       <Container>
-        <ul>
-          {allPokemon.map((pokemon, index) => {
-            const { artist, id, imageUrlHiRes, name } = pokemon;
-            return (
-              <li key={id}>
-                <h3>{name}</h3>
-                <img src={imageUrlHiRes} alt="Pokemon Cards!" />
-                <p>by: {artist}</p>
-              </li>
-            );
-          })}
-        </ul>
-        {/* <p className="gatsby-astronaut">
-          <img src={img_gatsby} alt="Build with Gatsby!" />
-        </p>
-        <h1>Gatsby Sass Starter</h1>
-        <p>
-          Welcome to your new Gatsby site.
-        </p>
-        <p>
-          Now go build something great.
-        </p>
-        <h2>Still Getting Started?</h2>
-        <p>
-          Run the following in your terminal!
-        </p>
-        <pre>
-          <code>
-            gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-sass
-          </code>
-        </pre> */}
+        <main>
+          <ul className="pokedex">
+            {allPokemon.map((pokemon, index) => {
+              const { artist, id, imageUrlHiRes, name } = pokemon;
+              return (
+                <li key={id}>
+                  <h3>{name}</h3>
+                  <img className="card" src={imageUrlHiRes} alt="Pokemon Cards!" />
+                  <p>by: {artist}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </main>
       </Container>
     </Layout>
   );
