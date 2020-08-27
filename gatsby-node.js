d420d5784914c33932b0ee0d272ed89a45017754
Jake Schaffer <jakecr8guru@gmail.com>
2020-08-27 14:28:27 -0500

    building gatsby-node.js file with a pokeapi

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,35 @@
 const fetch = require('node-fetch');
 
-exports.sourceNodes = async () => {
-  console.log('hellow world!');
+const NODE_TYPE = 'Pokemon';
+
+exports.sourceNodes = async ({ actions, createNodeId, creatContentDigest }) => {
+  const { createNode } = actions;
+
+  const response = await fetch(
+    "https://api.pokemontcg.io/v1/cards"
+  );
+  const pokemon = await response.json();
+  
+  // const { results = [] } = json;
+
+  // const pokemon = await Promise.all(results.map(async result => {
+  //   const { url } = result;
+  //   const pokeResponse = await fetch(url);
+  //   return await pokeResponse.json();
+  // }));
+
+  pokemon.forEach((node, index) => {
+    creatNode({
+      ...node,
+      id: creatNodeId(`${NODE_TYPE}-${node.id}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: NODE_TYPE,
+        content: JSON.stringify(node),
+        contentDigest: createContentDigest(node)
+      }
+    })
+  })
+  
 }
