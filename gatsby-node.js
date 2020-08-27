1bda6803dabfa877cf2975cf0f224d00a82fecb7
Jake Schaffer <jakecr8guru@gmail.com>
2020-08-27 15:15:16 -0500

    gastby-node.j is starting to work how I want

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,26 +2,50 @@ const fetch = require('node-fetch');
 
 const NODE_TYPE = 'Pokemon';
 
-exports.sourceNodes = async ({ actions, createNodeId, creatContentDigest }) => {
+exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => {
   const { createNode } = actions;
+
+  // --------------------
 
   const response = await fetch(
     "https://api.pokemontcg.io/v1/cards"
   );
-  const pokemon = await response.json();
+
+  const json = await response.json();
+
+  const { cards = [] } = json;
+
+  const pokemon = await Promise.all(
+    cards.map(async (result) => {
+      const pokeResponse = await result;
+      return await pokeResponse;
+    })
+  );
+
+  // ------------------
+
+  // const response = await fetch(
+  //   "https://pokeapi.co/api/v2/pokemon/"
+  // );
+
+  // const json = await response.json();
   
   // const { results = [] } = json;
 
-  // const pokemon = await Promise.all(results.map(async result => {
-  //   const { url } = result;
-  //   const pokeResponse = await fetch(url);
-  //   return await pokeResponse.json();
-  // }));
+  // const pokemon = await Promise.all(
+  //   results.map(async (result) => {
+  //     const { url } = result;
+  //     const pokeResponse = await fetch(url);
+  //     return await pokeResponse.json();
+  //   })
+  // );
+
+  // ---------------------
 
   pokemon.forEach((node, index) => {
-    creatNode({
+    createNode({
       ...node,
-      id: creatNodeId(`${NODE_TYPE}-${node.id}`),
+      id: createNodeId(`${NODE_TYPE}-${node.id}`),
       parent: null,
       children: [],
       internal: {
